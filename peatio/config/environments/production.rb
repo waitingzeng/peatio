a0609232923b292bd7662e298f582d5d5bf148fd
waiting <ttwait@gmail.com>
2018-05-17 13:07:20 +0800

    update

--- peatio/config/environments/production.rb
+++ peatio/config/environments/production.rb
@@ -69,6 +69,8 @@ Peatio::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV["URL_HOST"], protocol: ENV['URL_SCHEMA'] }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           ENV["SMTP_PORT"],
